52a44efb84c0a0b26880b2f78655cabb42150145
sleepypenguin763 <114171626+sleepypenguin763@users.noreply.github.com>
2022-12-09 11:45:47 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -12,3 +12,18 @@ openButton.addEventListener("click", function () {
   modal.classList.toggle("closed");
   modalOverlay.classList.toggle("closed");
 });
+
+$(document).on("scroll", function() {
+  var pageTop = $(document).scrollTop();
+  var pageBottom = pageTop + $(window).height();
+  var tags = $(".fadeIn");
+
+  for (var i = 0; i < tags.length; i++) {
+    var tag = tags[i];
+    if ($(tag).position().top < pageBottom) {
+      $(tag).addClass("visible");
+    } else {
+      $(tag).removeClass("visible");
+    }
+  }
+});
